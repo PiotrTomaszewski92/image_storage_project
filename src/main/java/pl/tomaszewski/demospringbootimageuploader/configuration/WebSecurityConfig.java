45d21b04207190e49dc720f1760f6c40f651d7ec
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-10 17:52:19 +0200

    solve problem with vaadin - connection problem

--- src/main/java/pl/tomaszewski/demospringbootimageuploader/configuration/WebSecurityConfig.java
+++ src/main/java/pl/tomaszewski/demospringbootimageuploader/configuration/WebSecurityConfig.java
@@ -42,7 +42,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/test2").hasRole("ADMIN")
                 .antMatchers("/uploadImage").hasRole("ADMIN")
         .and()
-        .formLogin().permitAll();
+        .formLogin().permitAll()
+        .and()
+        .csrf().disable().headers().disable();
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
