71a02827d1d29383f51610ad8bda20057699326b
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-10 15:10:18 +0200

    implement configuration for user Service

--- src/main/java/pl/tomaszewski/demospringbootimageuploader/configuration/WebSecurityConfig.java
+++ src/main/java/pl/tomaszewski/demospringbootimageuploader/configuration/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.tomaszewski.demospringbootimageuploader.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,18 +8,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.tomaszewski.demospringbootimageuploader.service.UserDetailsServiceImpl;
 
 import java.util.Collections;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig (UserDetailsServiceImpl userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(
-                new User("Jan",passwordEncoder().encode("jan123"), Collections.singleton(new SimpleGrantedAuthority("user"))));
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
--- src/main/java/pl/tomaszewski/demospringbootimageuploader/service/UserDetailsServiceImpl.java
+++ src/main/java/pl/tomaszewski/demospringbootimageuploader/service/UserDetailsServiceImpl.java
@@ -1,10 +1,22 @@
 package pl.tomaszewski.demospringbootimageuploader.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.tomaszewski.demospringbootimageuploader.repository.UserRepo;
 
-public class UserDetaulService implements UserDetailsService {
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepo userRepo;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return null;
