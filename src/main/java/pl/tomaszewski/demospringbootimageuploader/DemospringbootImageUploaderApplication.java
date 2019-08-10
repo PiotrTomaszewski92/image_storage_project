19e116f70c8b531ca2319e5dd61eaf0c1bc4702e
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-10 12:36:53 +0200

    first commit with sql configuration

---
+++ src/main/java/pl/tomaszewski/demospringbootimageuploader/DemospringbootImageUploaderApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszewski.demospringbootimageuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringbootImageUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringbootImageUploaderApplication.class, args);
+    }
+
+}
