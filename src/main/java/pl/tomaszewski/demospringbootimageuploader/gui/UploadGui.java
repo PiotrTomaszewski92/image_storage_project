757974f80449828d46a053293beedb59f29228cd
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-10 17:35:48 +0200

    implements gui to show webside with form

---
+++ src/main/java/pl/tomaszewski/demospringbootimageuploader/gui/UploadGui.java
@@ -0,0 +1,33 @@
+package pl.tomaszewski.demospringbootimageuploader.gui;
+
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import pl.tomaszewski.demospringbootimageuploader.service.ImageUploaderService;
+
+import java.io.IOException;
+
+@Route("/uploadImage")
+public class UploadGui extends VerticalLayout {
+
+    ImageUploaderService imageUploaderService;
+
+
+
+    public UploadGui(ImageUploaderService imageUploader) {
+        this.imageUploaderService = imageUploader;
+
+        TextField textField = new TextField();
+        Button button = new Button("upload");
+
+        button.addClickListener(buttonClickEvent -> {
+            try {
+                imageUploader.uploadImage(textField.getValue());
+            } catch (IOException e) {
+                //todo
+            }
+        });
+    }
+}
