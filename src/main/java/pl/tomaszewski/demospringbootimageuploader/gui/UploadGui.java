2d13807b414d28f35218af9d92c1101d8bc2fbcb
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-10 18:08:06 +0200

    concrete problem related with vaadin

--- src/main/java/pl/tomaszewski/demospringbootimageuploader/gui/UploadGui.java
+++ src/main/java/pl/tomaszewski/demospringbootimageuploader/gui/UploadGui.java
@@ -2,9 +2,12 @@ package pl.tomaszewski.demospringbootimageuploader.gui;
 
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 import pl.tomaszewski.demospringbootimageuploader.service.ImageUploaderService;
 
 import java.io.IOException;
@@ -15,19 +18,25 @@ public class UploadGui extends VerticalLayout {
     ImageUploaderService imageUploaderService;
 
 
-
-    public UploadGui(ImageUploaderService imageUploader) {
+    @Autowired
+    public UploadGui(ImageUploaderService imageUploader){
         this.imageUploaderService = imageUploader;
+
+        Label label = new Label();
 
         TextField textField = new TextField();
         Button button = new Button("upload");
 
         button.addClickListener(buttonClickEvent -> {
-            try {
-                imageUploader.uploadImage(textField.getValue());
-            } catch (IOException e) {
-                //todo
-            }
+
+                String uploadedImageUrl = imageUploader.uploadImage(textField.getValue());
+                Image image = new Image(uploadedImageUrl,"Nie ma obrazka");
+                label.setText("Wgrany obrazek: "+uploadedImageUrl);
+                add(label);
+                add(image);
         });
+
+        add(textField);
+        add(button);
     }
 }
